51b375bdb7a29e6b763eeccdbd07151c6f824336
pab1m <pavlosenchak14@gmail.com>
2023-04-10 15:15:56 +0300

    modified new and btn files

--- btn.py
+++ btn.py
@@ -3,14 +3,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 btn_for_new = KeyboardButton(text="Для новачків")
 btn_stats = KeyboardButton(text="Моя статистика")
 btn_help = KeyboardButton(text="Допомога")
-
-# kb_help = InlineKeyboardMarkup(row_width=1)
-# btn4 = InlineKeyboardButton(text="Test", callback_data='test')
-# kb_help.row(btn4)
-
 kb_help = ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
 kb_help.row(btn_for_new, btn_stats, btn_help)
-
 
 
 btn_agents = KeyboardButton(text="Агенти")
@@ -21,16 +15,12 @@ new_player = ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
 new_player.add(btn_agents, btn_maps, btn_gun, btn_lobby)
 
 
-
-
-btn_controller = KeyboardButton(text="КОНТРОЛЕР (CONTROLLER)")
+btn_controller = KeyboardButton(text="СПЕЦІАЛІСТ (CONTROLLER)")
 btn_duelist = KeyboardButton(text="ДУЕЛЯНТ (DUELIST)")
 btn3_sentinel = KeyboardButton(text="ВАРТОВИЙ/СТРАЖ (SENTINEL)")
 btn_initiator = KeyboardButton(text="ІНІЦІАТОР (INITIATOR)")
 agents = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 agents.add(btn_controller, btn_duelist, btn3_sentinel, btn_initiator, btn_for_new)
-
-
 
 
 btn_brim = KeyboardButton(text="BRIMSTONE")
@@ -45,7 +35,6 @@ controller.add(btn_brim, btn_viper, btn_omen, btn_astra, btn_harbor).add(btn_age
 info_brim = InlineKeyboardMarkup(row_width=2)
 btn_info_brim = InlineKeyboardButton(text="Біографія BRIMSTONE", callback_data='bio_brim')
 btn_abilities_brim = InlineKeyboardButton(text="Здібності BRIMSTONE", callback_data='abilities_brim')
-
 info_brim.add(btn_info_brim, btn_abilities_brim)
 
 
@@ -54,8 +43,13 @@ btn_brim_q = InlineKeyboardButton(text="Запалювальна граната 
 btn_brim_e = InlineKeyboardButton(text="Небесний дим (E)", callback_data='brim_e')
 btn_brim_c = InlineKeyboardButton(text="Маячок-стимулятор (C)", callback_data='brim_c')
 btn_brim_x = InlineKeyboardButton(text="Орбітальний удар (X)", callback_data='brim_x')
-
 abilities_brim.add(btn_brim_q, btn_brim_e, btn_brim_c, btn_brim_x)
+
+
+info_viper = InlineKeyboardMarkup(row_width=2)
+btn_info_viper = InlineKeyboardButton(text="Біографія VIPER", callback_data='bio_viper')
+btn_abilities_viper = InlineKeyboardButton(text="Здібності VIPER", callback_data='abilities_viper')
+info_viper.add(btn_info_viper, btn_abilities_viper)
 
 
 abilities_viper = InlineKeyboardMarkup(row_width=2)
@@ -63,7 +57,56 @@ btn_viper_q = InlineKeyboardButton(text="Отруйна хмара (Q)", callbac
 btn_viper_e = InlineKeyboardButton(text="Токсична завіса (E)", callback_data='viper_e')
 btn_viper_c = InlineKeyboardButton(text="Укус змії (C)", callback_data='viper_c')
 btn_viper_x = InlineKeyboardButton(text="Гніздо гадюки (X)", callback_data='viper_x')
+abilities_viper.add(btn_viper_q, btn_viper_e, btn_viper_c, btn_viper_x)
 
-abilities_brim.add(btn_viper_q, btn_viper_e, btn_viper_c, btn_viper_x)
+
+info_omen = InlineKeyboardMarkup(row_width=2)
+btn_info_omen = InlineKeyboardButton(text="Біографія OMEN", callback_data='bio_omen')
+btn_abilities_omen = InlineKeyboardButton(text="Здібності OMEN", callback_data='abilities_omen')
+info_omen.add(btn_info_omen, btn_abilities_omen)
 
 
+abilities_omen = InlineKeyboardMarkup(row_width=2)
+btn_omen_q = InlineKeyboardButton(text="Параноя (Q)", callback_data='omen_q')
+btn_omen_e = InlineKeyboardButton(text="Темний покров (E)", callback_data='omen_e')
+btn_omen_c = InlineKeyboardButton(text="Прихований крок (C)", callback_data='omen_c')
+btn_omen_x = InlineKeyboardButton(text="З тіні (X)", callback_data='omen_x')
+abilities_omen.add(btn_omen_q, btn_omen_e, btn_omen_c, btn_omen_x)
+
+
+info_astra = InlineKeyboardMarkup(row_width=2)
+btn_info_astra = InlineKeyboardButton(text="Біографія ASTRA", callback_data='bio_astra')
+btn_abilities_astra = InlineKeyboardButton(text="Здібності ASTRA", callback_data='abilities_astra')
+info_astra.add(btn_info_astra, btn_abilities_astra)
+
+
+abilities_astra = InlineKeyboardMarkup(row_width=2)
+btn_astra_q = InlineKeyboardButton(text="Вибух наднової (Q)", callback_data='astra_q')
+btn_astra_e = InlineKeyboardButton(text="Туманність (E)", callback_data='astra_e')
+btn_astra_c = InlineKeyboardButton(text="Гравітаційний колодязь (C)", callback_data='astra_c')
+btn_astra_x = InlineKeyboardButton(text="Космічний розрив (X)", callback_data='astra_x')
+abilities_astra.add(btn_astra_q, btn_astra_e, btn_astra_c, btn_astra_x)
+
+
+info_harbor = InlineKeyboardMarkup(row_width=2)
+btn_info_harbor = InlineKeyboardButton(text="Біографія HARBOR", callback_data='bio_harbor')
+btn_abilities_harbor = InlineKeyboardButton(text="Здібності HARBOR", callback_data='abilities_harbor')
+info_harbor.add(btn_info_harbor, btn_abilities_harbor)
+
+
+abilities_harbor = InlineKeyboardMarkup(row_width=2)
+btn_harbor_q = InlineKeyboardButton(text="Залив (Q)", callback_data='harbor_q')
+btn_harbor_e = InlineKeyboardButton(text="Гребень хвилі (E)", callback_data='harbor_e')
+btn_harbor_c = InlineKeyboardButton(text="Каскад (C)", callback_data='harbor_c')
+btn_harbor_x = InlineKeyboardButton(text="Розрахунок курсу (X)", callback_data='harbor_x')
+abilities_harbor.add(btn_harbor_q, btn_harbor_e, btn_harbor_c, btn_harbor_x)
+
+
+btn_phoenix = KeyboardButton(text="PHOENIX")
+btn_reyna = KeyboardButton(text="REYNA")
+btn_jett = KeyboardButton(text="JETT")
+btn_raze = KeyboardButton(text="RAZE")
+btn_yoru = KeyboardButton(text="YORU")
+btn_neon = KeyboardButton(text="NEON")
+duelist = ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+duelist.add(btn_phoenix, btn_reyna, btn_jett, btn_raze, btn_yoru, btn_neon).add(btn_agents)
--- new_main.py
+++ new_main.py
@@ -37,37 +37,24 @@ async def new_people(messsage: types.Message):
 
 @dp.message_handler(lambda message: 'Агенти' in message.text)
 async def new_people(message: types.Message):
-    await bot.delete_message(message.chat.id, message.message_id - 1)
+    # await bot.delete_message(message.chat.id, message.message_id - 1)
     # await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    await bot.delete_message(message.chat.id, message.message_id)
+    # await bot.delete_message(message.chat.id, message.message_id)
 
-    await message.answer('Агенти:', reply_markup=agents)
+    delete = await message.answer('Агенти:', reply_markup=agents)
+    # await delete.delete()
+    # await bot.delete_message(message.chat.id, message.message_id)
 
 
 
-
-@dp.message_handler(lambda message: "КОНТРОЛЕР (CONTROLLER)" in message.text)
+@dp.message_handler(lambda message: "СПЕЦІАЛІСТ (CONTROLLER)" in message.text)
 async def new_people(messsage: types.Message):
-    await bot.send_message(messsage.from_user.id, 'КОНТРОЛЕР (CONTROLLER)', reply_markup=controller)
+    await bot.send_message(messsage.from_user.id, 'СПЕЦІАЛІСТ (CONTROLLER)', reply_markup=controller)
     # await messsage.answer('КОНТРОЛЕР (CONTROLLER)', reply_markup=controller)
 
 
-# @dp.message_handler(lambda message: "BRIMSTONE" in message.text)
-
-# @dp.callback_query_handler(text='BRIMSTONE')
-# async def new_people(call: types.CallbackQuery):
-#     photo = types.InputFile('brimstone.png')
-#     # await bot.send_photo(chat_id=messsage.from_user.id, photo=photo)
-#
-#     await call.answer('good')
-#
-#     # await bot.send_message(messsage.from_user.id, 'КОНТРОЛЕР (CONTROLLER)', reply_markup=controller)
-#     # await call.answer(call.from_user.id, reply_markup=btn_info_brim)
-#     await call.answer('test0', reply_markup=tt())
-
-
 @dp.message_handler(text=['BRIMSTONE'])
-async def test(message: types.Message):
+async def brim(message: types.Message):
     photo = types.InputFile('agents_images/brimstone.png')
     await bot.send_photo(chat_id=message.from_user.id, photo=photo)
     photo.clean()
@@ -80,116 +67,285 @@ async def test(message: types.Message):
         await callback.answer()
 
     @dp.callback_query_handler(text='abilities_brim')
-    async def bio_brim(callback: types.CallbackQuery):
+    async def ab_brim(callback: types.CallbackQuery):
         await callback.message.answer('Оберіть здібність:', reply_markup=abilities_brim)
         await callback.answer()
 
-
     @dp.callback_query_handler(text='brim_q')
-    async def bio_brim(callback: types.CallbackQuery):
+    async def ab_brim_q(callback: types.CallbackQuery):
         with open("agents_images/brimstone.png", 'rb') as brim:
             await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис абілки')
+        ab = Brim().abilities()
+        await callback.message.answer(ab['q'])
         await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
         await callback.answer()
-
 
     @dp.callback_query_handler(text='brim_e')
-    async def bio_brim(callback: types.CallbackQuery):
+    async def ab_brim_e(callback: types.CallbackQuery):
         with open("agents_images/brimstone.png", 'rb') as brim:
             await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис aбілки')
+        ab = Brim().abilities()
+        await callback.message.answer(ab['e'])
         await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
         await callback.answer()
-
 
     @dp.callback_query_handler(text='brim_c')
-    async def bio_brim(callback: types.CallbackQuery):
+    async def ab_brim_c(callback: types.CallbackQuery):
         with open("agents_images/brimstone.png", 'rb') as brim:
             await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис aбілки')
+        ab = Brim().abilities()
+        await callback.message.answer(ab['c'])
         await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
         await callback.answer()
 
-
     @dp.callback_query_handler(text='brim_x')
-    async def bio_brim(callback: types.CallbackQuery):
+    async def ab_brim_x(callback: types.CallbackQuery):
         with open("agents_images/brimstone.png", 'rb') as brim:
             await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис ульти')
+        ab = Brim().abilities()
+        await callback.message.answer(ab['x'])
         await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
         await callback.answer()
 
 
 @dp.message_handler(text=['VIPER'])
-async def test(message: types.Message):
-    photo = types.InputFile('agents_images/viper.png.png')
+async def viper(message: types.Message):
+    photo = types.InputFile('agents_images/viper.png')
     await bot.send_photo(chat_id=message.from_user.id, photo=photo)
     photo.clean()
-    await message.answer('Оберіть категорію для ознайомлення:', reply_markup=info_brim)
+    await message.answer('Оберіть категорію для ознайомлення:', reply_markup=info_viper)
 
-    @dp.callback_query_handler(text='bio_brim')
-    async def bio_brim(callback: types.CallbackQuery):
-        Brim_bio = Brim().bio()
-        await callback.message.answer(Brim_bio)
+    @dp.callback_query_handler(text='bio_viper')
+    async def bio_viper(callback: types.CallbackQuery):
+        Viper_bio = Viper().bio()
+        await callback.message.answer(Viper_bio, parse_mode='html')
         await callback.answer()
 
-    @dp.callback_query_handler(text='abilities_brim')
-    async def bio_brim(callback: types.CallbackQuery):
-        await callback.message.answer('Оберіть здібність:', reply_markup=abilities_brim)
-        await callback.answer()
-
-
-    @dp.callback_query_handler(text='brim_q')
-    async def bio_brim(callback: types.CallbackQuery):
-        with open("agents_images/brimstone.png", 'rb') as brim:
-            await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис абілки')
-        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
+    @dp.callback_query_handler(text='abilities_viper')
+    async def ab_viper(callback: types.CallbackQuery):
+        await callback.message.answer('Оберіть здібність:', reply_markup=abilities_viper)
         await callback.answer()
 
 
-    @dp.callback_query_handler(text='brim_e')
-    async def bio_brim(callback: types.CallbackQuery):
-        with open("agents_images/brimstone.png", 'rb') as brim:
-            await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис aбілки')
-        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
+    @dp.callback_query_handler(text='viper_q')
+    async def ab_viper_q(callback: types.CallbackQuery):
+        with open("agents_images/viper.png", 'rb') as viper:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=viper)
+        ab = Viper().abilities()
+        await callback.message.answer(ab['q'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_viper)
         await callback.answer()
 
 
-    @dp.callback_query_handler(text='brim_c')
-    async def bio_brim(callback: types.CallbackQuery):
-        with open("agents_images/brimstone.png", 'rb') as brim:
-            await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис aбілки')
-        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
+    @dp.callback_query_handler(text='viper_e')
+    async def ab_viper_e(callback: types.CallbackQuery):
+        with open("agents_images/viper.png", 'rb') as viper:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=viper)
+        ab = Viper().abilities()
+        await callback.message.answer(ab['e'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_viper)
         await callback.answer()
 
 
-    @dp.callback_query_handler(text='brim_x')
-    async def bio_brim(callback: types.CallbackQuery):
-        with open("agents_images/brimstone.png", 'rb') as brim:
-            await bot.send_photo(chat_id=callback.from_user.id, photo=brim)
-
-        await callback.message.answer(text='опис ульти erghjegheruhghreiuhiuehveuihviuh')
-        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_brim)
+    @dp.callback_query_handler(text='viper_c')
+    async def ab_viper_c(callback: types.CallbackQuery):
+        with open("agents_images/viper.png", 'rb') as viper:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=viper)
+        ab = Viper().abilities()
+        await callback.message.answer(ab['c'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_viper)
         await callback.answer()
 
 
+    @dp.callback_query_handler(text='viper_x')
+    async def ab_viper_x(callback: types.CallbackQuery):
+        with open("agents_images/viper.png", 'rb') as viper:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=viper)
+        ab = Viper().abilities()
+        await callback.message.answer(ab['x'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_viper)
+        await callback.answer()
 
 
-# @dp.callback_query_handler(text=['test'])
-# async def test(callback: types.CallbackQuery):
-#     await callback.message.answer('sho ty?')
-#     await callback.answer()
+@dp.message_handler(text=['OMEN'])
+async def omen(message: types.Message):
+    photo = types.InputFile('agents_images/omen.png')
+    await bot.send_photo(chat_id=message.from_user.id, photo=photo)
+    photo.clean()
+    await message.answer('Оберіть категорію для ознайомлення:', reply_markup=info_omen)
+
+    @dp.callback_query_handler(text='bio_omen')
+    async def bio_omen(callback: types.CallbackQuery):
+        Omen_bio = Omen().bio()
+        await callback.message.answer(Omen_bio, parse_mode='html')
+        await callback.answer()
+
+    @dp.callback_query_handler(text='abilities_omen')
+    async def ab_omen(callback: types.CallbackQuery):
+        await callback.message.answer('Оберіть здібність:', reply_markup=abilities_omen)
+        await callback.answer()
+
+    @dp.callback_query_handler(text='omen_q')
+    async def ab_omen_q(callback: types.CallbackQuery):
+        with open("agents_images/omen.png", 'rb') as omen:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=omen)
+        ab = Omen().abilities()
+        await callback.message.answer(ab['q'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_omen)
+        await callback.answer()
+
+
+    @dp.callback_query_handler(text='omen_e')
+    async def ab_omen_e(callback: types.CallbackQuery):
+        with open("agents_images/omen.png", 'rb') as omen:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=omen)
+        ab = Omen().abilities()
+        await callback.message.answer(ab['e'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_omen)
+        await callback.answer()
+
+
+    @dp.callback_query_handler(text='omen_c')
+    async def ab_omen_c(callback: types.CallbackQuery):
+        with open("agents_images/omen.png", 'rb') as omen:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=omen)
+        ab = Omen().abilities()
+        await callback.message.answer(ab['c'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_omen)
+        await callback.answer()
+
+
+    @dp.callback_query_handler(text='omen_x')
+    async def ab_omen_x(callback: types.CallbackQuery):
+        with open("agents_images/omen.png", 'rb') as omen:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=omen)
+        ab = Omen().abilities()
+        await callback.message.answer(ab['x'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_omen)
+        await callback.answer()
+
+
+@dp.message_handler(text=['ASTRA'])
+async def astra(message: types.Message):
+    photo = types.InputFile('agents_images/astra.png')
+    await bot.send_photo(chat_id=message.from_user.id, photo=photo)
+    photo.clean()
+    await message.answer('Оберіть категорію для ознайомлення:', reply_markup=info_astra)
+
+    @dp.callback_query_handler(text='bio_astra')
+    async def bio_astra(callback: types.CallbackQuery):
+        Astra_bio = Astra().bio()
+        await callback.message.answer(Astra_bio, parse_mode='html')
+        await callback.answer()
+
+    @dp.callback_query_handler(text='abilities_astra')
+    async def ab_astra(callback: types.CallbackQuery):
+        await callback.message.answer('Оберіть здібність:', reply_markup=abilities_astra)
+        await callback.answer()
+
+    @dp.callback_query_handler(text='astra_q')
+    async def ab_astra_q(callback: types.CallbackQuery):
+        with open("agents_images/astra.png", 'rb') as astra:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=astra)
+        ab = Astra().abilities()
+        await callback.message.answer(ab['q'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_astra)
+        await callback.answer()
+
+
+    @dp.callback_query_handler(text='astra_e')
+    async def ab_astra_e(callback: types.CallbackQuery):
+        with open("agents_images/astra.png", 'rb') as astra:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=astra)
+        ab = Astra().abilities()
+        await callback.message.answer(ab['e'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_astra)
+        await callback.answer()
+
+
+    @dp.callback_query_handler(text='astra_c')
+    async def ab_astra_c(callback: types.CallbackQuery):
+        with open("agents_images/astra.png", 'rb') as astra:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=astra)
+        ab = Astra().abilities()
+        await callback.message.answer(ab['c'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_astra)
+        await callback.answer()
+
+
+    @dp.callback_query_handler(text='astra_x')
+    async def ab_astra_x(callback: types.CallbackQuery):
+        with open("agents_images/astra.png", 'rb') as astra:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=astra)
+        ab = Astra().abilities()
+        await callback.message.answer(ab['x'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_astra)
+        await callback.answer()
+
+
+@dp.message_handler(text=['HARBOR'])
+async def harbor(message: types.Message):
+    photo = types.InputFile('agents_images/harbor.png')
+    await bot.send_photo(chat_id=message.from_user.id, photo=photo)
+    photo.clean()
+    await message.answer('Оберіть категорію для ознайомлення:', reply_markup=info_harbor)
+
+    @dp.callback_query_handler(text='bio_harbor')
+    async def bio_harbor(callback: types.CallbackQuery):
+        Harbor_bio = Harbor().bio()
+        await callback.message.answer(Harbor_bio, parse_mode='html')
+        await callback.answer()
+
+    @dp.callback_query_handler(text='abilities_harbor')
+    async def ab_harbor(callback: types.CallbackQuery):
+        await callback.message.answer('Оберіть здібність:', reply_markup=abilities_harbor)
+        await callback.answer()
+
+    @dp.callback_query_handler(text='harbor_q')
+    async def ab_harbor_q(callback: types.CallbackQuery):
+        with open("agents_images/harbor.png", 'rb') as harbor:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=harbor)
+        ab = Harbor().abilities()
+        await callback.message.answer(ab['q'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_harbor)
+        await callback.answer()
+
+    @dp.callback_query_handler(text='harbor_e')
+    async def ab_harbor_e(callback: types.CallbackQuery):
+        with open("agents_images/harbor.png", 'rb') as harbor:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=harbor)
+        ab = Harbor().abilities()
+        await callback.message.answer(ab['e'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_harbor)
+        await callback.answer()
+
+    @dp.callback_query_handler(text='harbor_c')
+    async def ab_harbor_c(callback: types.CallbackQuery):
+        with open("agents_images/harbor.png", 'rb') as harbor:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=harbor)
+        ab = Harbor().abilities()
+        await callback.message.answer(ab['c'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_harbor)
+        await callback.answer()
+
+    @dp.callback_query_handler(text='harbor_x')
+    async def ab_harbor_x(callback: types.CallbackQuery):
+        with open("agents_images/harbor.png", 'rb') as harbor:
+            await bot.send_photo(chat_id=callback.from_user.id, photo=harbor)
+        ab = Harbor().abilities()
+        await callback.message.answer(ab['x'])
+        await callback.message.answer(text='Оберіть здібність:', reply_markup=abilities_harbor)
+        await callback.answer()
+
+
+@dp.message_handler(lambda message: "ДУЕЛЯНТ (DUELIST)" in message.text)
+async def new_people(messsage: types.Message):
+    await bot.send_message(messsage.from_user.id, "ДУЕЛЯНТ (DUELIST)", reply_markup=duelist)
+    # await messsage.answer('КОНТРОЛЕР (CONTROLLER)', reply_markup=controller)
+
+
+
+
 
 
 executor.start_polling(dp, skip_updates=True)
