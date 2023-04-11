e01c53c0714bc6cd708ed3fe333f0c22bc9a5e5b
Pavlo <pavlosenchak14@gmail.com>
2023-04-11 21:27:24 +0300

    api

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import os
+
+import match as match
 import requests
 import valorant
 
-api_key = "RGAPI-5ff5e39e-06e5-4439-a39d-29725610291b"
+api_key = "RGAPI-6622d638-c0ef-4fc3-b913-36b2e588f7cb"
 
-client = valorant.Client(api_key, locale="en-US")
 
-skins = client.get_skins()
-
-# agents = client.get_characters()
-
-# acts = client.get_acts()
-
-# act = acts.get(isActive=True)
+client = valorant.Client(api_key, locale=None)
+# client = valorant.Client(api_key, locale=None)
 
 account = client.get_user_by_name("Pab1m#1606")
 
-# match = account.matchlist().history.find(queueId="competitive")
-# match = account.matchlist()
+matches = account.matchlist().history.find(queueId="competitive")
+matches = matches.get()
 
-# print(match)
+print(matches.teams)
+
+
+
+# match = account.matchlist().history.find(queueId="competitive")
+# match = match.get()
+# for team in match.teams:
+#     print(f"{team.teamId} Team's Ranks: ")
+#
+#     players = match.players.get_all(teamId=team.teamId)
+#
+#     for player in players:
+#         print(f"\t{player.gameName} - {player.rank}")
+
+
+
+
+# skins = client.get_skins()
+#
+# name = []
+#
+# pages = client.get_leaderboard(size=50)
+#
+# print(pages.players[1].gameName)
+
+
+
+
+# for i in act:
+#     print(i)
+# print(acts)
+# print(act)
+
+
+
+# account = client.get_user_by_name(name='Pab1m#1606', route='europe')
+
+# match = account.matchlist().history.find(queueId="competitive")
+# matchs = account.matchlist()
+
 
 
 # lb = client.get_leaderboard(size=100)
@@ -27,12 +62,12 @@ account = client.get_user_by_name("Pab1m#1606")
 # for name in res:
 #     print(f"{name.gameName}, {name.rankedRating}")
 
-name = input("Search a Valorant Skin Collection: ")
 
-results = skins.find_all(name=lambda x: name.lower() in x.lower())
 
-print("\nResults: ")
-for skin in results:
-    print(f"\t{skin.name.ljust(50)} ({skin.localizedNames['es-ES']})")
+# name = input("Search a Valorant Skin Collection: ")
+# results = skins.find_all(name=lambda x: name.lower() in x.lower())
+# print("\nResults: ")
+# for skin in results:
+#     print(f"\t{skin.name.ljust(50)} {skin.localizedNames['ru-RU']}")
 
 
